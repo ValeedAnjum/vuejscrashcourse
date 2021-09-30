3365c4201679850cba0c45e3472f045df5b4343d
valeedanjum <allinone644@gmail.com>
2021-09-30 14:37:48 +0500

    how to add events

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <button @click="showTask()" :style="{background:color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+    export default {
+        name:"Button",
+        props:{
+            text:String,
+            color:String
+        },
+        methods:{
+            showTask(){
+                console.log('I am triggered');
+            }
+        }
+    }
+</script>
+
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,19 @@
 <template>
 <header>
     <h1>{{title}}</h1>
+    <Button  text="Add Task" color="red" />
 </header>
 </template>
 
 <script>
+import Button from './Button.vue'
 export default {
     name: 'Header',
     props: {
-        title:{
-            type:String,
-            defualt:'I am default'
-        }
+        title:String
+    },
+    components:{
+        Button,
     }
 }
 </script>
